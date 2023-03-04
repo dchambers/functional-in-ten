75ef5fe4441f4bee4bc3778413bde9c905fb4c6b
Dominic Chambers <dominic.chambers@gmail.com>
2023-03-04 22:12:03 +0000

    Functional in ten

---
+++ lesson1.js
@@ -0,0 +1,15 @@
+/*
+ * TASK:
+ * Write a program that allows a builder to determine whether they have enough money to carpet a
+ * group of houses that they've constructed.
+ *
+ * DETAILS:
+ * 1. Carpet costs £7.49/sqm.
+ * 2. Each house has a carpeted area of 84 sqm.
+ * 3. There are 20 houses to carpet.
+ * 4. There's £21,500 in remaining funds.
+ *
+ * HOMEWORK:
+ * 1. Figure out the greatest number of houses we could carpet with the available money.
+ * 2. Figure out highest cost of carpet we could afford if we needed to carpet 30 houses.
+ */
