0c7e28cfd799c86025f9ae19ca24923f16711b55
Dominic Chambers <dominic.chambers@gmail.com>
2023-03-04 22:12:03 +0000

    Functional in ten

---
+++ lesson6.ts
@@ -0,0 +1,21 @@
+/*
+ * TASK:
+ * Write a program that takes a list of numbers (e.g. `[3, 4, 5]`), and displays to the user a
+ * number report, with the following form:
+ *
+ * Number Report for numbers 3, 4, 5:
+ *
+ * 3 + 4 + 5 = 12 (Sum)
+ * 3 × 4 × 5 = 60 (Product)
+ *
+ * BONUS:
+ * Turn the program into a function that can be called with different lists of numbers.
+ *
+ * HOMEWORK:
+ * 1. Create an `average` function that takes a list of numbers, and returns the mean average of those numbers.
+ *    - e.g. `average([3, 4, 5])` would be `4`, since the sum of three, four and five is twelve, and dividing
+ *      twelve by the number of items in the list (three) gives you four.
+ *    - Hint: you can use the `sum` function that you create previously to help you.
+ * 2. Add the average of the numbers to the number report so that it looks like this:
+ *    - `(3 + 4 + 5) ÷ 3 = 4 (Average)`
+ */
