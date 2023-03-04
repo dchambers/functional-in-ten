057589c91a46deaf7476dd939b7037d6191d6518
Dominic Chambers <dominic.chambers@gmail.com>
2023-03-04 22:12:03 +0000

    Functional in ten

---
+++ lesson8.ts
@@ -0,0 +1,11 @@
+/**
+ * TASK:
+ * Create functions that would have allowed us to have better reports in previous lessons:
+ *
+ * 1. A `displayNumber` function that comma delimits numbers so they are easier to read.
+ * 2. A `delimitList` function that delimits lists with a comma for all but the final two items in
+ *    the list, which it delimits with an ampersand.
+ *
+ * BONUS:
+ * 1. Show how `import` and `export` can be used to have those reports make use of the functions defined here.
+ */
