4aacea2deffa2cf38b27126578f644e2989c0198
Dominic Chambers <dominic.chambers@gmail.com>
2023-03-04 22:12:03 +0000

    Functional in ten

---
+++ lessonX.ts
@@ -0,0 +1,6 @@
+/*
+ * TASK:
+ * Use objects and arrays to re-write the cake tin program in a way that makes it more generic,
+ * so that it's closer to being something that could work for any user without them having to
+ * modify code. A `cakeTins` definition is provided to get you started.
+ */
