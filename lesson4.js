3734ddd0bd3234d5b77f8a7bf486c7ae70d9f3ec
Dominic Chambers <dominic.chambers@gmail.com>
2023-03-04 22:12:03 +0000

    Functional in ten

---
+++ lesson4.js
@@ -0,0 +1,29 @@
+/*
+ * TASK:
+ * Write a program that takes a list of numbers, and displays to the user a report detailing the
+ * subset of numbers that are odd, that are even, and the value of those numbers if doubled or
+ * halved.
+ *
+ * Assuming the list of numbers [1, 2, 3] it should generate a report as follows:
+ *
+ * Number report for numbers 1, 2, 3:
+ *
+ * Filtered Numbers:
+ * 1, 3 (Odd Numbers)
+ * 2 (Even Numbrers)
+ *
+ * Mapped Numbers:
+ * 2, 4, 6 (Numbers Doubled)
+ * 0.5, 1, 1.5 (Numbers Halved)
+ *
+ * BONUS:
+ * Add a 'Mapped AND Filtered' sections that would look as follows:
+ *
+ * Mapped AND Filtered:
+ * 2, 4, 6 (Even, Doubled Numbers)
+ *  (Odd, Doubled Numbers)
+ *
+ * HOMEWORK:
+ * 1. Add tripled and quadrupled numbers to the 'Mapped Numbers' section.
+ * 2. Add tripled and quadrupled numbers to the 'Mapped AND Filtered Numbers' section.
+ */
